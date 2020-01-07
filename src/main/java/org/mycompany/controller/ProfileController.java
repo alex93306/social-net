03530173e5ddd6a78766c942a2e043af6f317a02
alex.ramanovich <alex93306@gmail.com>
2020-01-07 15:34:07 +0300

    initial commit under message module

--- src/main/java/org/mycompany/controller/ProfileController.java
+++ src/main/java/org/mycompany/controller/ProfileController.java
@@ -56,10 +56,9 @@ public class ProfileController {
     }
 
     protected AppUser getCurrentApplicationUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Principal principal = (Principal) authentication.getPrincipal();
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Principal principal = (Principal) authentication.getPrincipal();
 
-        //todo:
-        return appUserService.findByEmail(principal.getUsername());
+        return new AppUser();
     }
 }
--- src/main/java/org/mycompany/entity/ResetPasswordToken.java
+++
@@ -1,4 +0,0 @@
-package org.mycompany.entity;
-
-public class ResetPasswordToken {
-}
